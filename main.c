84756862668ca5c7a144e61840bf6a2f0215bed5
neauoire <aliceffekt@gmail.com>
2023-09-05 11:16:03 -0700

    Abstracted generation

--- main.c
+++ main.c
@@ -5,20 +5,24 @@
 #include <stdio.h>
 
 int
+generate(char *s)
+{
+	hotp_context ctx;
+	ctx.secret = (const uint8_t *)s;
+	ctx.secretSize = sizeof(s) - 1;
+	ctx.hashFn = (void *(*)(const void *, size_t))method_two;
+	ctx.blockSize = 64;
+	ctx.outputLength = 20;
+	return totp(&ctx, 6);
+}
+
+int
 main(int argc, char **argv)
 {
 	if(argc == 1) {
 		fprintf(stderr, "usage: totp message\n");
 		return 1;
 	}
-
-	hotp_context ctx;
-	ctx.secret = (const uint8_t *)argv[1];
-	ctx.secretSize = sizeof(argv[1]) - 1;
-	ctx.hashFn = (void *(*)(const void *, size_t))method_two;
-	ctx.blockSize = 64;
-	ctx.outputLength = 20;
-
-	printf("%d\n", totp(&ctx, 6));
+	printf("%d\n", generate(argv[1]));
 	return 0;
 }
